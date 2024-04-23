42e149f2a35386f2b5745f9e1a505b96c4c9cead
Mohinur <157112134+Monusuela@users.noreply.github.com>
2024-04-23 16:40:57 +0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import { Fragment } from "react" 
+import { Fragment } from "react"
 import "./App.css"
+import Container from "./components/container";
+import { Route, Router, Link } from "react-router-dom";
+import { Flex } from "antd";
+// import header from "./layout/header"
+
 
 
 const App = () => {
   return (
     <Fragment>
       <header className="header">
-        
+        <Container>
+          <Flex>
+
+            <Flex >
+              <Flex>
+                {/* <Logo /> */}
+                <nav>
+                  <Link to={"/"} className="">Функции</Link>
+                  <Link to={"/"} className="">Кошельки</Link>
+                  <Link to={"/"} className="">Участие</Link>
+                  <Link to={"/"} className="">Ресурсы</Link>
+                  <Link to={"/"} className="">Новости</Link>
+                  <Link to={"/"} className="">О BTCA</Link>
+                </nav>
+              </Flex>
+            </Flex>
+          </Flex>
+        </Container>
       </header>
     </Fragment>
   )

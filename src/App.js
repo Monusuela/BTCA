973abba255c9cc5163c5ea9bf2eea2e7eeb2e788
Mohinur <157112134+Monusuela@users.noreply.github.com>
2024-04-30 16:59:40 +0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { Fragment } from "react"
-import "./App.css"
+import "./App.scss"
 import Container from "./components/container";
-import { Route, Router, Link } from "react-router-dom";
+import { Route, Routes, Link } from "react-router-dom";
 import { Flex, Button } from "antd";
-import { TfiWorld} from "react-icons/tfi";
+import { TfiWorld } from "react-icons/tfi";
 import { RiMoneyDollarCircleLine } from "react-icons/ri";
 import { TbCoinBitcoin } from "react-icons/tb";
+import { Content, Footer } from "antd/es/layout/layout";
 // import { BsCurrencyBitcoin } from "react-icons/bs";
 
 
@@ -14,33 +15,52 @@ import { TbCoinBitcoin } from "react-icons/tb";
 const App = () => {
   return (
     <Fragment>
-      <header className="header">
+      <header className="header" backgroundColor="">
         <Container>
+          <Flex className="header-main">
           <Flex>
+                <nav className="header-main__menu">
 
-            <Flex >
-              <Flex>
-                {/* <Logo /> */}
-                <nav>
-                  <TbCoinBitcoin/>
+                  <TbCoinBitcoin style={{color: "white", size: "100px"}}/>
+
                   <Link to={"/"} className="">Функции</Link>
                   <Link to={"/"} className="">Кошельки</Link>
                   <Link to={"/"} className="">Участие</Link>
                   <Link to={"/"} className="">Ресурсы</Link>
                   <Link to={"/"} className="">Новости</Link>
                   <Link to={"/"} className="">О BTCA</Link>
-                </nav>
 
-                <Flex>
-                  {/* Заменить images */}
-                  <Button className=""  icon={<TfiWorld style={{color: "red"}}/> }>Rus</Button>
-                  <Button className="" icon={<RiMoneyDollarCircleLine style={{color: "white", backgroundColor: "orange"}}/>}>10.834</Button>
-                </Flex>
-              </Flex>
-            </Flex>
+                </nav>
+                
+          </Flex>
+          <Flex className="header-main__end">
+
+                  <Button className="header-main__end-button" icon={<TfiWorld style={{ color: "white" }} />}>Rus</Button>
+                  <Button className="header-main__end-button" backgroundColor="gradients/input70" icon={<RiMoneyDollarCircleLine style={{ color: "white", backgroundColor: "orange", borderRadius:"100%" }} />}>10.834</Button>
+             
+             
+          </Flex>
           </Flex>
         </Container>
       </header>
+
+      <Content>
+        <Routes>
+          {/* <Route path="/" element={} /> */}
+
+          {/* <Route path="/" element={} /> */}
+
+          {/* <Route path="/" element={} /> */}
+
+          {/* <Route path="/" element={} /> */}
+
+          {/* <Route path="/" element={} /> */}
+
+          {/* <Route path="/" element={} /> */}
+        </Routes>
+      </Content>
+
+      <Footer />
     </Fragment>
   )
 }

3d6e255337ccf0a3229e4441e56855b07a9e99a1
Monusuela <mohinurshakirdjanova@gmail.com>
2024-05-25 19:36:30 +0500

    #fix subscribe

--- src/modules/subscribe.jsx
+++ src/modules/subscribe.jsx
@@ -1,12 +1,27 @@
 import { Button, Col, Flex, Input } from "antd";
 import Title from "antd/es/skeleton/Title";
-import { Fragment } from "react"
+import { Fragment, useState } from "react"
 import { Link } from "react-router-dom";
 
+
+//  import React, { useEffect, Fragment, useState } from "react";
+// import { useMutation, useSubscription, gql } from "@apollo/client";
+
+// import OnlineUser from "./OnlineUser";
+
 const Subscribe = () => {
+
+const [userEmail, setUserEmail] = useState("")
+
+const addSubscribe = () => {
+  localStorage.setItem("userEmail", userEmail);
+  window.location.reload()
+}
+
+
   return (
     <Fragment>
-      <Flex className="Block-area">
+      {!localStorage.getItem("userEmail") ? (<Flex className="Block-area">
         <Col className="Block-main">
           <Title className="Block-area-title">
             Никогда не пропускайте обновления
@@ -18,18 +33,25 @@ const Subscribe = () => {
           </p>
           <Flex className="Block-area-2">
             <Input className="Block-area-2-input"
+            id="userEmail"
+            type="Email"
               placeholder={"Ваш e-mail"}
+              value={userEmail}
+              onChange={(e) => setUserEmail(e.target.value)}
             ></Input>
             <div className="">
               <Button
                 className="Block-area-2-button"
+                onClick={addSubscribe}
               >
                 <Link to="/questions">Подписаться</Link>
               </Button>
             </div>
           </Flex>
         </Col>
-      </Flex>
+      </Flex> ) : (
+        <div></div>
+      )}
     </Fragment>
   )
 }
